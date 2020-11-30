c322967ab4a9c029e92bc9d587b646e5913835d7
rubens-lavor <haryson07@gmail.com>
2020-11-30 02:07:27 -0300

    output ok

--- Lista.hpp
+++ Lista.hpp
@@ -1,15 +1,26 @@
+#ifndef LISTA_H
+#define LISTA_H
+
 #include <iostream>
 #include <memory>
 #include <vector>
 
-class Conta;
+class Pessoa;
+using namespace std;
 
-template <class Conta>
+template <class T>
 class Lista {
+    public:
     //Tipo* outros;
-    std::unique_ptr<Conta[]> contas;
+    std::unique_ptr<T[]> contas;
     int maximo, top, tam=1;
-    std::vector<Conta>contasss;
+    std::vector<T>contasss;
+    Pessoa *ptr;
+
+    template <typename>
+    friend std::ostream &operator<<(std::ostream &, Lista<T> &);
+    template <typename>
+    friend std::istream &operator>>(std::istream &, Lista<T> &);
 
     /*
     Lista(Conta &a){
@@ -18,17 +29,36 @@ class Lista {
         contasss.push_back(a);
     }
     */
-   public:
-   void operator+(Conta c){
+   //public:
+   void operator+(T &c){
        contasss.push_back(c);
    }
 
-   void incluir(Conta c){
+   void incluir(T &c){
        contasss.push_back(c);
    }
-   
-    template <typename>
-    friend std::ostream &operator<<(std::ostream &, const Lista<Conta> &);
-    template <typename>
-    friend std::istream &operator>>(std::istream &, Lista<Conta> &);
+
+   vector<T> getLista(){
+    return contasss;
+   }
+
+    
 };
+
+template <class T>
+istream &operator>>(istream &input, Lista<T> &a) {
+    return input;
+}
+
+template <class T>
+ostream &operator<<(ostream &output, Lista<T> &list) {
+    output << "Tamanho da lista:" << list.contasss.size() << "\n";
+    for (size_t i = 0; i < list.contasss.size(); i++)
+    {
+        output << "Conta " << i << " - Nome " << list.contasss[i].getCorrentista().getNome() << "\n";
+    }
+    
+    return output;
+}
+
+#endif
