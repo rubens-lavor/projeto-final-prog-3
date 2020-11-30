1c18fb830ae8b850284713ab2ac101cf87816274
rubens-lavor <haryson07@gmail.com>
2020-11-30 15:44:21 -0300

    concluído

--- Lista.hpp
+++ Lista.hpp
@@ -1,68 +1,56 @@
 #ifndef LISTA_H
 #define LISTA_H
 
+#include <algorithm>
 #include <iostream>
 #include <memory>
 #include <vector>
 
-/*
-class Pessoa;
-using namespace std;
-*/
-
 template <class T>
 class Lista {
-    //Tipo* outros;
-    std::unique_ptr<T[]> contas;
-    int maximo, top, tam=1;
-    std::vector<T>contasss;
+    std::vector<T> contas;
 
     template <typename>
     friend std::ostream &operator<<(std::ostream &, Lista<T> &);
-    template <typename>
-    friend std::istream &operator>>(std::istream &, Lista<T> &);
 
     /*
-    Lista(Conta &a){
-        contas = new Conta[tam];
-        contas[tam] = a;
-        contasss.push_back(a);
-    }
+    template <typename>
+    friend std::istream &operator>>(std::istream &, Lista<T> &);
     */
+
    public:
-   void operator+(T &c){
-       contasss.push_back(c);
-   }
+    void operator+(T &c) {
+        contas.push_back(c);
+    }
 
-   void incluir(T &c){
-       contasss.push_back(c);
-   }
+    vector<T> getLista() const {
+        return contas;
+    }
 
-   vector<T> getLista(){
-    return contasss;
-   }
-
-    
+    T &operator[](size_t indice) {
+        return contas[indice];
+    }
 };
-
+/*
 template <class T>
 istream &operator>>(istream &input, Lista<T> &a) {
     return input;
 }
+*/
 
 template <class T>
 ostream &operator<<(ostream &output, Lista<T> &list) {
     output << "Tamanho da lista:" << list.getLista().size() << "\n";
     string nome{};
-    //int numero;
-    for (size_t i = 0; i < list.getLista().size(); i++)
-    {   
-        nome = list.getLista().at(i).getCorrentista().getNome();
-        // colocar numero da conta
+    int numero;
 
-        output << "Conta " << i << " - Nome: " << nome << "\n";
+    for (size_t i = 0; i < list.getLista().size(); i++) {
+        nome = list[i].getCorrentista().getNome();
+        numero = list[i].getNumero();
+
+        output << "Conta " << numero << " - Nome: " << nome << "\n";
     }
-    
+
     return output;
 }
 
