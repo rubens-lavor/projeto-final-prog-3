46a23be22b150e9a6f22ab4ca514fa910b77ea73
rubens-lavor <haryson07@gmail.com>
2020-12-04 23:53:41 -0300

    template ok

--- codigos/Lista.hpp
+++ codigos/Lista.hpp
@@ -8,7 +8,8 @@
 
 template <class T>
 class Lista {
-    std::vector<T> contas;
+    std::vector<T*> contas{3};
+    size_t top = 0;
 
     template <typename>
     friend std::ostream &operator<<(std::ostream &, Lista<T> &);
@@ -17,20 +18,46 @@ class Lista {
     template <typename>
     friend std::istream &operator>>(std::istream &, Lista<T> &);
     */
+   /*
+   std::unique_ptr<Tipo[]> outros;
+    int maximo, top;
 
    public:
-
-    vector<T> getLista() const {
+    Lista(unsigned capacidade = 20) : top{-1} {
+//        outros = new Tipo[capacidade];
+        outros = std::make_unique<Tipo[]>(capacidade);
+        maximo = capacidade;
+    }
+*/
+    public:
+    /*
+    * retorna a lista
+    */
+   
+    vector<T*> getLista() const {
         return contas;
     }
 
-    void operator+(T &c) {
-        contas.push_back(c);
+
+    /*
+     * adiciona uma conta a lista
+    */
+    void operator+(T *c) {
+        contas[top]=c;
+        top++;
+
     }
 
-    T &operator[](size_t indice) {
+    void insere(T &c){
+        contas[top]=&c;
+        top++;
+    }
+
+    
+    T* operator[](size_t indice) const {
         return contas[indice];
     }
+
 };
 /*
 template <class T>
@@ -40,14 +67,14 @@ istream &operator>>(istream &input, Lista<T> &a) {
 */
 
 template <class T>
-ostream &operator<<(ostream &output, Lista<T> &list) {
+ostream &operator<<(ostream &output, const Lista<T> &list) {
     output << "Tamanho da lista:" << list.getLista().size() << "\n";
     string nome{};
     int numero;
 
     for (size_t i = 0; i < list.getLista().size(); i++) {
-        nome = list[i].getCorrentista().getNome();
-        numero = list[i].getNumero();
+        nome = list[i]->getCorrentista().getNome();
+        numero = list[i]->getNumero();
 
         output << "Conta " << numero << " - Nome: " << nome << "\n";
     }
