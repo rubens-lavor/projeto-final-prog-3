6dbdc7055b13d94314322f50e21cc33fe4fc59c2
rubens-lavor <haryson07@gmail.com>
2020-12-05 14:40:10 -0300

    documentação conta.h ok

--- codigos/conta.h
+++ codigos/conta.h
@@ -2,7 +2,6 @@
 #define CONTA_H
 #include <iostream>
 #include <string>
-using namespace std;
 
 #include "pessoa.h"
 
@@ -13,33 +12,26 @@ contas bancárias
 
 */
 
+/*
+- A classe Conta é base para as classes ContaComum, ContaPoupança e ContaEspecial, por meio do conceito de herança
+*/
+class Conta {
+   private:
+    static int contasCriadas;  //contador de objetos da classe Conta
 
-
-
-
-//class Pessoa; // se eu tirar o identificador do tipo não é definido
-//verificar o pq da razão de se colocar o class Pessoa
-
-class Conta
-{
-private:
-    static int contasCriadas; //contador de objetos da classe Conta
-    string senha="";
-    //int tipo = 0;
     /*
-    static int DEPOSITAR;
-    static int SACAR;
+    - atributo que armazena a senha da conta
     */
-    
-protected:
+    string senha = "";
 
+   protected:
     /*
     o atributo número é utilizado para referenciar o número da conta.
     */
     int numero;
 
     /*
-    o atributo correntista, do tipo Pessoa é utilizado para referenciar um correntista.
+    o atributo correntista, do tipo Pessoa, é utilizado para referenciar um correntista.
     */
     Pessoa *correntista;
 
@@ -48,69 +40,113 @@ protected:
     */
     float saldo{};
 
-    enum class Contas{    COMUM,    POUPANCA,    ESPECIAL};
+    //enum class Contas{    COMUM,    POUPANCA,    ESPECIAL};
 
-public:
-
+   public:
     /*
-    Construtor padão da Classe Conta
+        Construtor padrão da Classe Conta
     */
     Conta();
 
-
     /*
     Construtor sobrecarregado da classe Conta
-    Uso:
-        Conta *conta = new ContaComum(12346,pessoa,150,10);
+
     Parametros:
-        int numero, Pessoa correntista, float saldo, float valor
+        int numero, Pessoa correntista, float saldo, string senha
     Onde:
-        numero -> inteiro que identifica o número da conta
-        correntista -> objeto do tipo Pessoa que identifica o correntista da conta
-        saldo -> float que identifica o saldo inicial
-        valor -> float que identifica... 
+        numero --> inteiro que identifica o número da conta
+        correntista --> objeto do tipo Pessoa que identifica o correntista da conta
+        saldo --> float que identifica o saldo inicial
+        senha --> string indetifica a senha da conta 
     */
     Conta(int numero, Pessoa &correntista, float saldo, string senha = "123");
 
+    /*
+        destrutor virual padrão da Classe Conta
+    */
     virtual ~Conta();
 
-    bool validacao (string chave) const;
-
-    int getNumero();
-    void setNumero(int num);
-    
-    
-    Pessoa getCorrentista();
-    void setCorrentista(Pessoa &correntista);
-    
-    float getSaldo();
-    void setSaldo(float saldo);
-
-    void depositar(float valor);
-    
-    //virtual int getTipo () const;
-
-    virtual void info() const;
-
-    virtual bool sacar(float valor); 
-    // é um método polimorfico
-
+    /*
+        - string chave representa a senha passada para verificação/autenticação
+        - returna true se chave for válida; caso contrário false.
+    */
+    bool validacao(string chave) const;
 
     /*
-        Exemplo:
-
-        struct Base { 
-            virtual void foo() {} 
-        };
-        struct Derived: Base { 
-            void foo() override {} 
-        };
-
+        método que retorna o número da conta
     */
-    virtual bool movimentar(float valor, int operacao);   
+    int getNumero();
+
+    /*
+        - método que atualiza o número da conta
+        - método usado pelo construtor
+    */
+    void setNumero(int num);
+
+    /*
+        retorna um ponteiro para um objeto Pessoa
+    */
+    Pessoa getCorrentista();
+
+    /*
+        - método que atribue um endereço de Pessoa ao atributo conrrentista
+        - método usado pelo construtor
+    */
+    void setCorrentista(Pessoa &correntista);
+
+    /*
+    - método que retorna o saldo da conta
+    */
+    float getSaldo();
+
+    /*
+    - método que atualiza o saldo da conta
+    */
+    void setSaldo(float saldo);
+
+    /*
+    função que retorna o total de objetos instanciados
+    */
     int getNumeroTotalDeContas();
 
-};
+    /*
+        - O método verifica qual tipo de operação se deseja realizar e chama a função correspondente:
+        - caso operação == 0 ---> sacar(float)
+        - caso operação == 1 ---> depositar(float)
+        - em ambos os casos passando o valor recebido por paramentro
+        - Caso a operação tenha sido realizada retorna true, caso não seja possível, retorna false
+    */
+    bool movimentar(float valor, int operacao);
 
+    /*
+    - método que realiza depósito em conta
+    */
+    void depositar(float valor);
+
+    
+    /* Métodos polimorficos */
+
+    /*
+    - O método info() é sobrescrito nas classes ContaComum, ContaPoupança e ContaEspecial
+    - info() imprime informações referentes a determinada conta, tais como numero da conta, nome do correntista, saldo, entre outros.
+    */
+    virtual void info() const;
+
+    /*
+    - O método sacar() é sobrescrito na classe ContaEspecial
+    - Como conta especial possibilita saque mesmo sem saldo até determinado valor
+    - a verificação acontece que maneira particular.
+
+    - o método recebe um valor como parâmetro verifica se há saldo suficiente para realizar o saque,
+    - Se sim retorna true, caso contrário, false.
+    - na classe Conta, sacar() é implementado e as classes ContaComum e ContaPoupança herdam o método
+    - sem necessidade de sobrescreve-lo.
+
+    - sacar() é chamado dentro do método movimentar()
+    */
+    virtual bool sacar(float valor);
+
+    //virtual bool movimentar(float valor, int operacao);
+};
 
 #endif
